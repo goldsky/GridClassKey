13d7b23f1bfb2cbe449001dcba7d623db8a4843a
goldsky <goldsky@fastmail.fm>
2014-01-14 17:07:44 +0700

    [#13] fix container's Resource Overview

---
+++ core/components/gridclasskey/controllers/container/data.class.php
@@ -0,0 +1,9 @@
+<?php
+require_once MODX_MANAGER_PATH . 'controllers/default/resource/data.class.php';
+/**
+ * Loads the resource data page
+ *
+ * @package modx
+ * @subpackage manager.controllers
+ */
+class GridContainerDataManagerController extends ResourceDataManagerController {}
