56434fd6d33ef35c86e8b7e1659c4141efb87623
goldsky <goldsky.milis@gmail.com>
2017-05-14 16:47:47 +0700

    updated copyyear

--- core/components/gridclasskey/processors/mgr/cmp/children/batchactions.class.php
+++ core/components/gridclasskey/processors/mgr/cmp/children/batchactions.class.php
@@ -3,7 +3,7 @@
 /**
  * GridClassKey
  *
- * Copyright 2013 - 2016 by goldsky <goldsky@virtudraft.com>
+ * Copyright 2013 - 2017 by goldsky <goldsky@virtudraft.com>
  *
  * This file is part of GridClassKey, a custom class key for MODX
  * Revolution's Manager to hide child resources inside container's grid.
